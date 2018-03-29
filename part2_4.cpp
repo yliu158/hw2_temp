bb6e2cdeff78ff740f2c1ea4d892f7160ee0584d
Yang Liu <yliu158@binghamton.edu>
2018-03-29 16:24:02 -0400

    Probably final

--- part2_4.cpp
+++ part2_4.cpp
@@ -10,19 +10,17 @@ using namespace std;
 
 #define NUM_DIMENSIONS 16
 #define HIS_NUMS 100
+#define NUM_THREADS 4
 const int N = 15;
-const int num_hist_intervals = 10;
-alignas(32) static float points[N][NUM_DIMENSIONS];
 
 int main() {
 
   uniform_real_distribution<float> dist(-1, 1);
-  static int count = 0;
-  static int total = 0;
-  double start = omp_get_wtime();
-  static int histgram[HIS_NUMS];
+  static int histgram[NUM_DIMENSIONS][HIS_NUMS+1];
+  static int num_sampling[NUM_DIMENSIONS];
 
-  omp_set_num_threads(3);
+  omp_set_num_threads(NUM_THREADS);
+  double start = omp_get_wtime();
   #pragma omp parallel
   {
     int thread_id = omp_get_thread_num();
@@ -30,40 +28,28 @@ int main() {
   #pragma omp for
     for (int i = 0; i < N; ++i ) {
       float distance_sq_sum = 2.0;
-      float temp[NUM_DIMENSIONS];
       while (distance_sq_sum > 1.0) {
         distance_sq_sum = 0.0;
         for (int j = 0; j < NUM_DIMENSIONS; ++j) {
-          temp[j] = dist(eng);
-          distance_sq_sum += temp[j]*temp[j];
+          float ran_num = dist(eng);
+          distance_sq_sum += ran_num*ran_num;
+          if (distance_sq_sum <= 1.0 && num_sampling[j] < N) {
+            ++num_sampling[j];
+            ++histgram[j][(int)(sqrt(distance_sq_sum)*HIS_NUMS)];
+          }
+          if (distance_sq_sum > 1.0) break;
         }
-        ++ total;
       }
-      for (int k = 0; k < NUM_DIMENSIONS; ++k) {
-        points[i][k] = temp[k];
-      }
-      ++ histgram[(int)(distance_sq_sum*HIS_NUMS)-1];
-      ++ count;
-      printf("thread_id: %d  count: %d rejected: %d\n",thread_id ,count, total-count);
     }
-
   }
-  printf("rejected ratio: %f\n", (float)(total-count)/(float)(total));
   double end = omp_get_wtime();
-  printf("sampling time: %f\n", end-start);
+  printf("using time: %f for %d threads.\n", end-start, NUM_THREADS);
 
-  // for (int i = 0; i < N; ++i) {
-  //   for (int j = 0; j < NUM_DIMENSIONS; ++j) {
-  //     printf("%d   %f", i,points[i][j]);
-  //   }
-  //   printf("\n");
-  // }
-
-  for (int i = 0; i < HIS_NUMS; ++i) {
-    printf("%d  ", histgram[i]);
+  for (int i = 0; i < NUM_DIMENSIONS; ++i) {
+    for (int j = 0; j < HIS_NUMS; ++j) {
+      printf("%d", histgram[i][j]);
+    }
+    printf("\n");
   }
-  printf("\n");
-
-
   return 0;
 }
