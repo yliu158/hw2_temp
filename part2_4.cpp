b2ad86ff46dadd5b8359247bea8c6e1bd164c996
Yang Liu <yliu158@binghamton.edu>
2018-03-29 16:59:43 -0400

    changed out put

--- part2_4.cpp
+++ part2_4.cpp
@@ -6,25 +6,29 @@
 #include <iostream>
 #include <cassert>
 #include <omp.h>
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
 #define NUM_DIMENSIONS 16
 #define HIS_NUMS 100
 #define NUM_THREADS 4
-const int N = 15;
+const int N = 100;
 
 int main() {
 
   uniform_real_distribution<float> dist(-1, 1);
   static int histgram[NUM_DIMENSIONS][HIS_NUMS+1];
   static int num_sampling[NUM_DIMENSIONS];
+  static int nthreads;
 
-  omp_set_num_threads(NUM_THREADS);
+  // omp_set_num_threads(NUM_THREADS);
   double start = omp_get_wtime();
   #pragma omp parallel
   {
     int thread_id = omp_get_thread_num();
     default_random_engine eng(thread_id);
+    if (thread_id == 0) nthreads = omp_get_num_threads();
   #pragma omp for
     for (int i = 0; i < N; ++i ) {
       float distance_sq_sum = 2.0;
@@ -43,11 +47,11 @@ int main() {
     }
   }
   double end = omp_get_wtime();
-  printf("using time: %f for %d threads.\n", end-start, NUM_THREADS);
+  printf("using time: %f for %d threads.\n", end-start, nthreads);
 
-  for (int i = 0; i < NUM_DIMENSIONS; ++i) {
-    for (int j = 0; j < HIS_NUMS; ++j) {
-      printf("%d", histgram[i][j]);
+  for (int i = 0; i < HIS_NUMS; ++i) {
+    for (int j = 0; j < NUM_DIMENSIONS; ++j) {
+      cout << setfill(' ') << setw(4) << left << histgram[j][i];
     }
     printf("\n");
   }
